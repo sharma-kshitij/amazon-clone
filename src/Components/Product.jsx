0d3d0f3ef263c4c8b6547ef421ce77dc1acb7049
Kshitij Sharma <sharmakshitij250@gmail.com>
2022-08-02 13:50:02 +0530

    Finish Home page

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,22 +1,27 @@
 import "../Styles/Product.css";
+import StarIcon from "@mui/icons-material/Star";
 
-const Product = () => {
+const Product = ({ id, title, image, price, rating }) => {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p> {title} </p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong> {price} </strong>
         </p>
         <div className="product__rating">
-          <p>☆</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>
+                {" "}
+                <StarIcon />{" "}
+              </p>
+            ))}
         </div>
       </div>
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
-        alt=""
-      />
+      <img src={image} alt="" />
 
       <button>Add to Basket</button>
     </div>
